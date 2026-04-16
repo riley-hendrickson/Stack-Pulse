18558cabef0fb23b8737633da823d37a7a8b7194
Riley Hendrickson <rileyhe1@gmail.com>
2026-04-16 15:04:48 -0700

    first bit of progress

--- scraper-service/src/main/java/stackpulse/Main.java
+++
@@ -1,9 +0,0 @@
-package stackpulse;
-
-public class Main
-{
-    static void main()
-    {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ scraper-service/src/main/java/stackpulse/scraper/config/EnvLoader.java
@@ -0,0 +1,17 @@
+package stackpulse.scraper.config;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnvLoader
+{
+    public static Map<String, String> load(String filePath)
+    {
+        Map<String, String> env = new HashMap<>();
+
+        return env;
+    }
+}
