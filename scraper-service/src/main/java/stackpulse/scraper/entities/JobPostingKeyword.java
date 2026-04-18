7f50bc07b63c61887e4d45a3b0a5a1f63f564383
Riley Hendrickson <rileyhe1@gmail.com>
2026-04-18 14:21:19 -0700

    converting into a spring project and updating dependencies to match

---
+++ scraper-service/src/main/java/stackpulse/scraper/entities/JobPostingKeyword.java
@@ -0,0 +1,4 @@
+package stackpulse.scraper.entities;
+
+public class JobPostingKeyword {
+}
---
+++ scraper-service/src/main/java/stackpulse/scraper/repositories/JobPostingKeywordRepository.java
@@ -0,0 +1,4 @@
+package stackpulse.scraper.repositories;
+
+public class JobPostingKeywordRepository {
+}
