cec57141ef1e6cf5d15f09747ef4a0617e32bd4c
Riley Hendrickson <rileyhe1@gmail.com>
2026-04-24 14:01:34 -0700

    refactored main to implement commandlinerunner so we can run scraperservice's scrape method on startup, wrote scraper service class which calls client's fetchjobs method, deserializes the json response to adzunaresponse, then loops through all job postings, skipping already persisted postings and persists new ones via the appropriate repository

--- scraper-service/src/main/java/stackpulse/scraper/Main.java
+++ scraper-service/src/main/java/stackpulse/scraper/Main.java
@@ -1,13 +1,27 @@
 package stackpulse.scraper;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import stackpulse.scraper.services.ScraperService;
 
 @SpringBootApplication
-public class Main
+public class Main implements CommandLineRunner
 {
+    private final ScraperService scraperService;
+
+    public Main(ScraperService scraperService)
+    {
+        this.scraperService = scraperService;
+    }
     public static void main(String[] args)
     {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        scraperService.scrape();
     }
 }
--- scraper-service/src/main/java/stackpulse/scraper/services/ScraperService.java
+++ scraper-service/src/main/java/stackpulse/scraper/services/ScraperService.java
@@ -1,4 +1,55 @@
 package stackpulse.scraper.services;
 
-public class ScraperService {
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+import stackpulse.scraper.client.AdzunaClient;
+import stackpulse.scraper.entities.JobPosting;
+import stackpulse.scraper.models.AdzunaJobResult;
+import stackpulse.scraper.models.AdzunaResponse;
+import stackpulse.scraper.repositories.JobPostingRepository;
+
+import java.io.IOException;
+
+@Service
+public class ScraperService
+{
+    private final AdzunaClient adzunaClient;
+    private final JobPostingRepository jobPostingRepository;
+    private final ObjectMapper objectMapper;
+
+    public ScraperService(AdzunaClient adzunaClient, JobPostingRepository jobPostingRepository, ObjectMapper objectMapper)
+    {
+        this.adzunaClient = adzunaClient;
+        this.jobPostingRepository = jobPostingRepository;
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.objectMapper = objectMapper;
+    }
+
+    public void scrape()
+    {
+        try
+        {
+            String jobs = adzunaClient.fetchJobs("Backend Developer", 1);
+            AdzunaResponse response = objectMapper.readValue(jobs, AdzunaResponse.class);
+            for(AdzunaJobResult jobResult : response.getJobResults())
+            {
+                if(jobPostingRepository.existsByAdzunaId(jobResult.getId())) continue;
+
+                JobPosting newPosting = JobPosting.builder()
+                        .adzunaId(jobResult.getId())
+                        .title(jobResult.getTitle())
+                        .description(jobResult.getDescription())
+                        .company(jobResult.getCompany().getCompanyName())
+                        .source("adzuna")
+                        .build();
+
+
+                jobPostingRepository.save(newPosting);
+            }
+        } catch (IOException e)
+        {
+            System.out.println("Error while scraping jobs: " + e.getMessage());
+        }
+    }
 }
