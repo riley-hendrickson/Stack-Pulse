04fcbfbe88a7c0a279c0ff6c6c93454f6b7a0a60
Riley Hendrickson <rileyhe1@gmail.com>
2026-04-24 13:59:18 -0700

    adding service annotation to client class and injecting appid and appkey from application.properties using value annotations in client constructor

--- scraper-service/src/main/java/stackpulse/scraper/client/AdzunaClient.java
+++ scraper-service/src/main/java/stackpulse/scraper/client/AdzunaClient.java
@@ -2,15 +2,25 @@ package stackpulse.scraper.client;
 
 import lombok.RequiredArgsConstructor;
 import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
+@Service
 public class AdzunaClient
 {
     private final String appId;
     private final String appKey;
     private final OkHttpClient client;
+
+    public AdzunaClient(@Value("${adzuna.app-id}") String appId,
+                        @Value("${adzuna.app-key}") String appKey)
+    {
+        this.appId = appId;
+        this.appKey = appKey;
+        this.client = new OkHttpClient();
+    }
 
     public String fetchJobs(String query, int page) throws IOException
     {
