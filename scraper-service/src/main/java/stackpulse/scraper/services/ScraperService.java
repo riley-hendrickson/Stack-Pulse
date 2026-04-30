5992ec524de17b333ea187e2965b726065ab2596
Riley Hendrickson <rileyhe1@gmail.com>
2026-04-30 12:47:24 -0700

    wired KeywordMatchingService into ScraperService, after saving a newly constructed jobPosting, we run keyword match's findMatches method against the description and create a JobPostingKeyword object for each match, and persist them to the jobPostingKeyword repository

--- scraper-service/src/main/java/stackpulse/scraper/services/ScraperService.java
+++ scraper-service/src/main/java/stackpulse/scraper/services/ScraperService.java
@@ -6,34 +6,42 @@ import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 import stackpulse.scraper.client.MuseClient;
 import stackpulse.scraper.entities.JobPosting;
+import stackpulse.scraper.entities.JobPostingKeyword;
 import stackpulse.scraper.models.MuseJobResult;
 import stackpulse.scraper.models.MuseResponse;
+import stackpulse.scraper.repositories.JobPostingKeywordRepository;
 import stackpulse.scraper.repositories.JobPostingRepository;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class ScraperService
 {
     private final MuseClient museClient;
     private final JobPostingRepository jobPostingRepository;
+    private final JobPostingKeywordRepository jobPostingKeywordRepository;
     private final ObjectMapper objectMapper;
+    private final KeywordMatchingService keywordMatchingService;
 
-    public ScraperService(MuseClient museClient, JobPostingRepository jobPostingRepository, ObjectMapper objectMapper)
+    public ScraperService(MuseClient museClient, JobPostingRepository jobPostingRepository,
+                          ObjectMapper objectMapper, KeywordMatchingService keywordMatchingService,  JobPostingKeywordRepository jobPostingKeywordRepository)
     {
         this.museClient = museClient;
         this.jobPostingRepository = jobPostingRepository;
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         this.objectMapper = objectMapper;
+        this.keywordMatchingService = keywordMatchingService;
+        this.jobPostingKeywordRepository = jobPostingKeywordRepository;
     }
 
     public void scrape()
     {
         try
         {
-            for(int i = 0; i < 5; i++)
+            for(int i = 1; i <= 5; i++)
             {
-                String jobs = museClient.fetchJobs( i);
+                String jobs = museClient.fetchJobs(i);
                 MuseResponse response = objectMapper.readValue(jobs, MuseResponse.class);
                 for(MuseJobResult jobResult : response.getResults())
                 {
@@ -48,7 +56,16 @@ public class ScraperService
 
 
                     jobPostingRepository.save(newPosting);
-            }
+                    List<String> matches = keywordMatchingService.findMatches(newPosting.getDescription());
+                    for(String match : matches)
+                    {
+                        JobPostingKeyword newKeyword = JobPostingKeyword.builder()
+                                .jobPosting(newPosting)
+                                .keyword(match)
+                                .build();
+                        jobPostingKeywordRepository.save(newKeyword);
+                    }
+                }
             }
         } catch (IOException e)
         {
