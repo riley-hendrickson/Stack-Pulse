59d34d79d25ca409a70cd21e2f2f9086da06f52f
Riley Hendrickson <rileyhe1@gmail.com>
2026-05-03 11:59:46 -0700

    implemented eureka server for service discovery

---
+++ eureka-server/src/main/java/stackpulse/eurekaserver/Main.java
@@ -0,0 +1,17 @@
+package stackpulse.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
