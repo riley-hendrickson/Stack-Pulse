82b82c2735c6b248b7f4b4ac8f22049d38beda82
Riley Hendrickson <rileyhe1@gmail.com>
2026-06-11 17:34:20 -0700

    adding swagger for api documentation and easy demo of endpoints

---
+++ query-api-service/src/main/java/stackpulse/queryAPI/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package stackpulse.queryAPI.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig
+{
+    @Value("${app.server-url:http://localhost:8080}")
+    private String serverUrl;
+    @Bean
+    public OpenAPI stackPulseOpenAPI()
+    {
+        return new OpenAPI()
+                .info(new Info()
+                .title("StackPulse Query API")
+                .description("REST API for querying technology keyword trends from job postings")
+                .version("1.0.0"))
+                .servers(List.of(new Server().url("http://localhost:8080")));
+    }
+}
--- query-api-service/src/main/java/stackpulse/queryAPI/dtos/KeywordFrequencyDTO.java
+++ query-api-service/src/main/java/stackpulse/queryAPI/dtos/KeywordFrequencyDTO.java
@@ -1,5 +1,6 @@
 package stackpulse.queryAPI.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "A technology keyword and its frequency across job postings")
 public class KeywordFrequencyDTO
 {
+    @Schema(description = "The technology keyword", example = "java")
     private String keyword;
+
+    @Schema(description = "Number of job postings containing this keyword", example = "142")
     private long frequency;
 }
